0157198870a4ac3986450ba6ff5f8c8dcbb74368
khoriisti <khori.isti@gmail.com>
2020-11-06 00:35:40 +0700

    Tambah Prodi

--- application/views/administrator/jurusan.php
+++ application/views/administrator/jurusan.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
 	
 	<div class="alert alert-success" role="alert">
-    	<i class="fas fa-university"></i> Form Input Jurusan
+    	<i class="fas fa-university"></i> Jurusan
     </div>
     <?php echo $this->session->flashdata('pesan'); ?>
 
